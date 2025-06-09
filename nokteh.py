e5ec3a30b286faf5a78aa24ef79cfb1a2a0ca181
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-06-09 18:57:42 +0330

    add comment important

---
+++ nokteh.py
@@ -0,0 +1,17 @@
+# dar haleh hazer ma darim emtahan haramidahim
+# dar hein emtehan agar betavanim ma django ra ham mibinim
+# barnameh bad az emtehan ha beh in shekl ast
+# django
+# rest python
+# celery
+# pwa
+# important project and create best rezomeh
+# job
+# shop important vasaiel
+# shoroieh chanel youtube
+# learn ML => numpy pandas matplotlib
+# learn scikit-learn
+# learn tensorflow keras
+# learn spark
+# learn openCV
+# learn lungchen
