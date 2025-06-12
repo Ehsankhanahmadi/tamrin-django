24c011e423abbc9a33fee8eaa3dbd6e8d3aaf921
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-06-12 23:14:23 +0330

    tamrin dynamic urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home)
+    path("", views.home),
+    # dynamic urls => behtar ast paiin bashand te be error nakhorim
+    path("<nameblog>", views.blog),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def home(req):
     return HttpResponse("this is home page section blog")
+
+def blog(req,nameblog):
+    return HttpResponse(f"this is {nameblog} blog")
