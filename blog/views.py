638ee472b263600421bff888d6b496a8bdfb6332
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-06-13 20:36:38 +0330

    tamrin tag django

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render, HttpResponse
+from django.http import Http404
+
+# database test
+data = [
+    {
+        "name":"blog one",
+        "disc":"disc one for blog one",
+        "time":"2005/10/11"
+    },
+    {
+        "name":"blog two",
+        "disc":"disc two for blog twp",
+        "time":"2005/10/01"
+    },
+    {
+        "name":"blog three",
+        "disc":"disc thee for blog thee",
+        "time":"2005/05/11"
+    },
+]
 
 def home(req):
-    return HttpResponse("this is home page section blog")
+    # return HttpResponse("this is home page section blog")
+    contexthome = {"data":data}
+    return render(req,"blog/home.html",contexthome)
 
 def blog(req,nameblog):
     # return HttpResponse(f"this is {nameblog} blog")
-    context = {"namelog":nameblog}
-    return render(req,"blog/blog.html",context)
+    for blog in data:
+        if blog["name"] == nameblog:
+            context = {"namelog":blog}
+            return render(req,"blog/blog.html",context)
+    raise Http404("this blog not found")
