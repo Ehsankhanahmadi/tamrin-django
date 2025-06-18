536d63aa4dfce9451c5dfbdce30daa46b6ffde9b
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-06-18 17:09:09 +0330

    tamrin file static

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render , HttpResponse
 
 def home(req):
-    return HttpResponse("this is home page site")
+    # return HttpResponse("this is home page site")
+    return render(req , "home/home.html")
