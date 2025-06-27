802871af8cd396d4ae845efd167214200aa88d69
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-06-27 12:13:03 +0330

    add new comment

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.home),
     path("add", views.addblog),
     # dynamic urls => behtar ast paiin bashand te be error nakhorim
-    # "<int:id>" => find one data ba type number int
+    # "data/<int:id>" => find one data ba type number int
     path("<nameblog>", views.blogp),
 ]
