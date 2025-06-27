9822bc2fd03b80da994719c6ee7d3bf846ce266e
Ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2025-06-27 13:32:31 +0330

    tamrin media folder file

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ class blog(models.Model):
     title = models.CharField(max_length=50)
     desc = models.TextField()
     time = models.TimeField()
+    img = models.ImageField(null=True,upload_to="blogimg")
 
     def __str__(self):
         return self.title
